c3f1690dcd7a75fd5094b67885de7ae590e4e942
jazmin.ferreiro <jazmin.ferreiro@despegar.com>
2019-11-10 02:47:07 -0300

    default categories

--- server/migrations/20190519195540-create-active-principles.js
+++ server/migrations/20190519195540-create-active-principles.js
@@ -24,6 +24,12 @@ module.exports = {
         allowNull: false,
         type: Sequelize.DATE
       }
+    }).then(function () {
+      queryInterface.sequelize.query("insert into ActivePrinciples (id, name, information, createdAt, updatedAt) values (1,'Entrada', 'Entrada','2019-11-10 05:01:08','2019-11-10 05:01:08')");
+      queryInterface.sequelize.query("insert into ActivePrinciples (id, name, information, createdAt, updatedAt) values (2,'Plato principal', 'Plato principal','2019-11-10 05:01:08','2019-11-10 05:01:08')");
+      queryInterface.sequelize.query("insert into ActivePrinciples (id, name, information, createdAt, updatedAt) values (3,'Postre', 'Postre', '2019-11-10 05:01:08','2019-11-10 05:01:08')");
+      queryInterface.sequelize.query("insert into ActivePrinciples (id, name, information, createdAt, updatedAt) values (4,'Guarnicion', 'Guarnicion','2019-11-10 05:01:08','2019-11-10 05:01:08')");
+      queryInterface.sequelize.query("insert into ActivePrinciples (id, name, information, createdAt, updatedAt) values (5,'Bebida', 'Bebida', '2019-11-10 05:01:08','2019-11-10 05:01:08') ");
     });
   },
   down: (queryInterface, Sequelize) => {
